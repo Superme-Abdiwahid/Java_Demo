d65b2ff00576dae6682bd43739eaa9883355c98c
Superme-Abdiwahid <76221708+Superme-Abdiwahid@users.noreply.github.com>
2021-11-25 14:17:05 -0800

    Create demo

---
+++ hello.java
@@ -0,0 +1,11 @@
+// define a class Hello to begin programming in java
+public class Hello{
+ /* define a main method */
+  public static void main(String[] args){
+   /* print hello world to the console */
+    System.out.print("Hello World my first lines of code in java YEYEYEEY!!");
+  }
+  
+  
+  
+}
